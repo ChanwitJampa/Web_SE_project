87795d5a4774669df0960c391b50103052d3712b
ThanakornLZX <97593786+ThanakornLZX@users.noreply.github.com>
2022-03-22 03:14:50 +0700

    5

--- client/src/components/Organizationcomponent1.js
+++ client/src/components/Organizationcomponent1.js
@@ -8,11 +8,12 @@ const OrganizationComponent1 = () => {
     return ( 
       <div>
            <NavbarComponent />
-              <div className={"content-box"}>
-                <h1  > สถานประกอบการ</h1>
+              <div className={"contenthead-box"}>
+                <h1> สถานประกอบการ</h1>
                   <input id="disabledInput" type="text" placeholder="ค้นหาสถานประกอบการ" onChange={""} ></input>
                  <button type="submit" className="btn btn-color">ยืนยัน</button> 
               </div>
+
               <div className='content1-box'>
                  <button class="button button1">ข้อมูลสถานประกอบการ</button>
                  <button class="button button2">จัดการข้อมูลสถานประกอบการ</button>
