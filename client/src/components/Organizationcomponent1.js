614e480a9e31c42376476e43d88b5d8f5fb655e1
ThanakornLZX <97593786+ThanakornLZX@users.noreply.github.com>
2022-03-21 16:41:42 +0700

    ตาราง

--- client/src/components/Organizationcomponent1.js
+++ client/src/components/Organizationcomponent1.js
@@ -11,16 +11,21 @@ const OrganizationComponent1 = () => {
             
               <div className={"content-box"}>
                 <h1> สถานประกอบการ</h1>
-                <input id="disabledInput" type="text" placeholder="ค้นหาสถานประกอบการ" onChange={""} ></input>
-                <button type="submit" className="btn btn-color">ยืนยัน</button> 
+                  <input id="disabledInput" type="text" placeholder="ค้นหาสถานประกอบการ" onChange={""} ></input>
+                 <button type="submit" className="btn btn-color">ยืนยัน</button> 
               </div>
               <div className='content1-box'>
-              <button class="button button1">ข้อมูลสถานประกอบการ</button>
-              <button class="button button2">จัดการข้อมูลสถานประกอบการ</button>
-              <Link to="/organization">
-              <button  class="button button3">+ เพิ่มสถานประกอบการ</button>
-               </Link>
-              </div>   
+                 <button class="button button1">ข้อมูลสถานประกอบการ</button>
+                 <button class="button button2">จัดการข้อมูลสถานประกอบการ</button>
+                    <Link to="/organization">
+                      <button  class="button button3">+ เพิ่มสถานประกอบการ</button>
+                   </Link>
+                   <div className="content2-box" onClick={" "}></div>
+                   <div className="content3-box" onClick={" "}></div>
+                   <div className="content4-box" onClick={" "}></div>
+                   <div className="content5-box" onClick={" "}></div>
+                   <div className="content6-box" onClick={" "}></div>
+              </div>    
       </div>
     );
   };
